97ea3f81230d0b308e558e9258847c7230e9194e
Daniel Suárez <uo276467@uniovi.es>
2023-09-24 20:04:08 +0200

    Ejercicios sesión 1

--- app/src/main/java/com/example/favmovies/MainActivity.java
+++ app/src/main/java/com/example/favmovies/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -29,9 +32,68 @@ public class MainActivity extends AppCompatActivity {
                             .show();
             }
         });
+
+        //Recuperamos referencia al botón
+        Button btnEditar = findViewById(R.id.buttonEditar);
+        //Definimos listener
+        btnEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Spinner categoria = findViewById(R.id.spCategoria);
+                if(categoria.getSelectedItemPosition() == 0){
+                    Snackbar.make(findViewById(R.id.layoutPrincipal), R.string.msg_nueva_categoria,
+                                    Snackbar.LENGTH_LONG)
+                            .show();
+                }else{
+                    Snackbar.make(findViewById(R.id.layoutPrincipal), R.string.msg_editar_categoria,
+                                    Snackbar.LENGTH_LONG)
+                            .show();
+                }
+            }
+        });
     }
 
     private boolean validarCampos(){
-        return true;
+        EditText titulo = findViewById(R.id.txtTitulo);
+        EditText sinopsis = findViewById(R.id.txtSinopsis);
+        Spinner categoria = findViewById(R.id.spCategoria);
+        EditText duracion = findViewById(R.id.txtDuracion);
+        EditText fecha = findViewById(R.id.txtFecha);
+
+        StringBuilder mensaje = new StringBuilder();
+        mensaje.append(getString(R.string.error_campos_sin_rellenar));
+
+        boolean errors = false;
+
+        if(titulo.getText().toString().trim().length() == 0){
+            mensaje.append("\n" + getString(R.string.titulo));
+            errors = true;
+        }
+        if(sinopsis.getText().toString().length() == 0){
+            mensaje.append("\n" + getString(R.string.argumento));
+            errors = true;
+        }
+        if(categoria.getSelectedItemPosition() == 0){
+            mensaje.append("\n" + getString(R.string.categoria));
+            errors = true;
+        }
+        if(duracion.getText().toString().length() == 0){
+            mensaje.append("\n" + getString(R.string.duracion));
+            errors = true;
+        }
+        if(fecha.getText().toString().length() == 0){
+            mensaje.append("\n" + getString(R.string.fecha));
+            errors = true;
+        }
+
+        if(errors) {
+            Snackbar.make(findViewById(R.id.layoutPrincipal), mensaje.toString(),
+                            Snackbar.LENGTH_LONG)
+                    .show();
+            return false;
+        } else {
+            return true;
+        }
     }
+
 }
